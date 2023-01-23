a021bdab7420621e4e9847c58ab101f6fd604a41
EdaKocas <edaakocass02@gmail.com>
2023-01-23 16:36:01 +0300

    ChatPortal
    Angular İle mesajlaşma programı

---
+++ EdaChat/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'edashoppingcenter',
+    appId: '1:538063420212:web:c39133f040b6e146148361',
+    storageBucket: 'edashoppingcenter.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBcle_Zz0x0_hN9wGqRGDvzynKjKUh3Ttw',
+    authDomain: 'edashoppingcenter.firebaseapp.com',
+    messagingSenderId: '538063420212',
+    measurementId: 'G-2TGDXXS4CC',
+  },
+  production: true
+};
---
+++ EdaChat/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'edashoppingcenter',
+    appId: '1:538063420212:web:c39133f040b6e146148361',
+    storageBucket: 'edashoppingcenter.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBcle_Zz0x0_hN9wGqRGDvzynKjKUh3Ttw',
+    authDomain: 'edashoppingcenter.firebaseapp.com',
+    messagingSenderId: '538063420212',
+    measurementId: 'G-2TGDXXS4CC',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
